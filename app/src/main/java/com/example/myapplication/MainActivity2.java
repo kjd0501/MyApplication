9124ddc51bb5e23db9b7a0f9ec0f5c30c8635f27
karen <karen.dsouza@outlook.com>
2022-07-06 11:31:14 +0400

    additions

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -2,13 +2,35 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private Button drillMode;
+    private Button suctionCups;
+    private Button goToHomeOrientation;
+    private Button raiseEndEffector;
+    private Button fw1;
+    private Button fw2;
+    private Button fw3;
+    private Button bw1;
+    private Button bw2;
+    private Button bw3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        drillMode = (Button) findViewById(R.id.drillMode);
+        suctionCups = (Button) findViewById(R.id.suctionCups);
+        goToHomeOrientation = (Button) findViewById(R.id.Home);
+        raiseEndEffector = (Button) findViewById(R.id.raiseEE);
+        fw1 = (Button) findViewById(R.id.fw1);
+        fw2 = (Button) findViewById(R.id.fw2);
+        fw3 = (Button) findViewById(R.id.fw3);
+        bw1 = (Button) findViewById(R.id.bw1);
+        bw2 = (Button) findViewById(R.id.bw2);
+        bw3 = (Button) findViewById(R.id.bw3);
     }
 }
 
