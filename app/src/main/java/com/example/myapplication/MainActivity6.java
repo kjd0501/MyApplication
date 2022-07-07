02b41e0d4060e48c58f7c667b6fab008e4e2c0e5
karen <karen.dsouza@outlook.com>
2022-07-07 12:08:41 +0400

    recyclerView defined

--- app/src/main/java/com/example/myapplication/MainActivity6.java
+++ app/src/main/java/com/example/myapplication/MainActivity6.java
@@ -1,14 +1,19 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity6 extends AppCompatActivity {
+public class MainActivity6 extends AppCompatActivity{
+
+    private RecyclerView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main6);
+
+        list = (RecyclerView)findViewById(R.id.manipList);
     }
 }
