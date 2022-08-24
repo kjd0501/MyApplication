95231af0ed2c8a70853ed8c7e15a92aebcfcf1e2
karen <karen.dsouza@outlook.com>
2022-08-24 23:48:48 +0400

    update to toggle buttons

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 import java.io.DataOutputStream;
 import java.io.IOException;
